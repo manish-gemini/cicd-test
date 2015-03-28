acce2a51e5f545e5a87833582643f9a574013782
sajithgem <sajith@gemini-systems.net>
2015-03-28 01:49:13 -0400

    Gemini CLI BUILD automation with godeps

--- automatecli.sh
+++ automatecli.sh
@@ -1,55 +1,38 @@
+#!/bin/bash
 echo "Starting Gemini Cli Build..."
 
 echo "Enter the dir to checkout :"
 read dirToCheckout
-echo $dirToCheckout
 
 if [ ! -d $dirToCheckout ]
 then
-	echo "Enter Valid Directory, exit.."
-        exit
+	echo "Specified Directory does not exist., Creating the directory..."
+	mkdir -p $dirToCheckout
 fi
+
 cd $dirToCheckout
-git clone https://sajithgem@github.com/Gemini-sys/Gemini-CLI.git
+echo "Git UserID: "
+read gitID
+git clone https://$gitID@github.com/Gemini-sys/Gemini-CLI.git
 
-echo "SETUP Environment : 1 "
-echo "Skip Settingup the Environment : 2 "
-read setupEnv
-echo $setupEnv
+echo "setting env variables ..."
+export GOPATH=$dirToCheckout/Gemini-CLI
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
-if [ $setupEnv -eq 1 ]
-then
-	echo "setting env variables"
-
-	GOPATH=$dirToCheckout/Gemini-CLI
-	PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
-	echo $GOPATH
-	echo $PATH
-fi
- echo $GOPATH
- echo $PATH
-
-
-echo "GO TO CROSS COMIPLER..."
 echo "Enter the dir to clone cross compiler"
 read dirToCrossCompiler
-echo $dirToCrossCompiler
-echo "Printing GO To Cross Compiler...."
-echo $GOPATH
-echo $PATH
+
 mkdir -p $dirToCrossCompiler
 cd $dirToCrossCompiler
 git clone git://github.com/davecheney/golang-crosscompile.git
-echo "Printing GO To Cross Compiler...."
-echo $GOPATH
-echo $PATH
 
 source golang-crosscompile/crosscompile.bash
 go-crosscompile-build-all
+go get github.com/tools/godep
 
 cd $dirToCheckout/Gemini-CLI/src/github.com/gemini/
 
 godep restore
-go-linux-arm build
+go-linux-amd64 build
 go-windows-amd64 build
 
