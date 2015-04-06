d6eb67737779969fdce6e730d29c41435f068b17
gopal-gemini <gopalakrishnan@gemini-systems.net>
2015-04-06 16:06:40 +0530

    Create python-encryption.sh

---
+++ Jenkins/python-encryption.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#python encryption
+
+rm -rf ./replicate-transport-python
+rm -rf ./orchestrate
+rm -rf ./replicate
+rm -rf ./appdb
+
+find . -name "*.pyc" -print -exec rm -f {} \;
+
+python -m compileall .
+
+find . -name "*.py" -print -exec rm -f {} \;
+
+rm -rf ./jcommon
+rm -rf ./p2v_convert
