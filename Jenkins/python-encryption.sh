53b859c7b025e16d2ecbe36ebce79b9b17c325f2
gopal-gemini <gopalakrishnan@gemini-systems.net>
2015-04-28 19:01:25 +0530

    Update python-encryption.sh

--- Jenkins/python-encryption.sh
+++ Jenkins/python-encryption.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-
-#use this file to /var/lib/jenkins/python-encryption.sh and make sure your this path in build commands
 #python encryption
 
-rm -rf ./replicate-transport-python
-rm -rf ./orchestrate
-rm -rf ./replicate
-rm -rf ./appdb
+#rm -rf ./replicate-transport-python
+#rm -rf ./orchestrate
+#rm -rf ./replicate
+#rm -rf ./appdb
 
 find . -name "*.pyc" -print -exec rm -f {} \;
 
 python -m compileall .
-
+find . -name "*.pyc" -print -exec chmod +x {} \;
 find . -name "*.py" -print -exec rm -f {} \;
 
-rm -rf ./jcommon
-rm -rf ./p2v_convert
+#rm -rf ./jcommon
+#rm -rf ./p2v_convert
