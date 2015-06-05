134bb2c9c1068be404bd85b7bc825edc9579295e
sajithgem <sajith@gemini-systems.net>
2015-06-05 08:05:37 -0400

    Customer Deliverables Initial commit

---
+++ Customer/Gemini-predeploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Check for PreRequisite...."
+#CHECK FOR PREREQUISTE and LETS USER KNOW 
+
+#Print the Number of VCPUs
+
+echo "Number of CPU : "
+nproc
+
+echo "Total RAM Size in Bytes :"
+vmstat -s | grep 'total memory'
+
+echo "Check for Docker Version "
+if docker -v | grep 1.5 > /dev/null
+then
+	echo "Docker Version 1.5 exists"
+elif docker -v | grep 1.6 > /dev/null
+then
+	echo "Docker Version 1.6 exists"
+else 
+	yum -y update
+	yum -y install docker
+	systemctl enable docker.service
+	systemctl start docker.service
+fi
+
+echo "Flush Iptables"
+
+iptables -F
+
+echo "yum install -y git wget"
+
+
