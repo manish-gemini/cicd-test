c1d2254ad45691bbdc6dac72a5b663793d296ca5
sajithgem <sajith@gemini-systems.net>
2015-06-10 09:01:03 -0400

    Exit on Login Failures

--- Customer/Gemini-Chef.sh
+++ Customer/Gemini-Chef.sh
@@ -5,8 +5,12 @@ read -p "Do you want to continue Deploy Chef Container ? y or n " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Login to the Internal Registry"
-  docker login https://secure-registry.gsintlab.com
+  echo "Login to the Gemini Repo"
+  if !(docker login https://secure-registry.gsintlab.com)
+  then
+	echo "Try Running Again with Corrrect Login Credentials. ERROR: Login Failed...Exiting..."
+	exit
+  fi         
   echo "Pull Chef Server from Internal Registry..."
   docker pull secure-registry.gsintlab.com/gemini/gemini-chef
   echo "Continue to run chef ..."
