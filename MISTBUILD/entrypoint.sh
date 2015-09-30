146f11b49bc428ea7b34b100093468fd0add28be
sajithgem <sajith@gemini-systems.net>
2015-09-30 01:48:54 -0400

    Branch added

--- MISTBUILD/entrypoint.sh
+++ MISTBUILD/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -x
 
-#./gradleinstall.sh
 export PATH=$PATH:/opt/gradle-2.7/bin/
+echo 'Starting to clone MIST from Repo'
 git clone https://github.com/Gemini-sys/mist-cgp.git
+read -p "Enter the branch to checkout [master]:" branch
 cd mist-cgp
+if [ ! -z "$branch" ]; then
+    git checkout $branch
+fi
 /opt/gradle-2.7/bin/gradle build run:export.run
 
