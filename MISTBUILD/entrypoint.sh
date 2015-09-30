b7fe11fc4f96cd063acff40acd3ed444066b35e2
sajithgem <sajith@gemini-systems.net>
2015-09-30 01:56:18 -0400

    exit on clone failed

--- MISTBUILD/entrypoint.sh
+++ MISTBUILD/entrypoint.sh
@@ -3,6 +3,10 @@
 export PATH=$PATH:/opt/gradle-2.7/bin/
 echo 'Starting to clone MIST from Repo'
 git clone https://github.com/Gemini-sys/mist-cgp.git
+if [ $? -ne 0 ];then
+  echo "Clone failed..."
+  exit
+fi
 read -p "Enter the branch to checkout [master]:" branch
 cd mist-cgp
 if [ ! -z "$branch" ]; then
