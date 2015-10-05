5dabc6947c40e6880f42d573c25a936ce138ff80
sajithgem <sajith@gemini-systems.net>
2015-10-05 02:18:59 -0400

    updated the way ip is got in chef deploy in customer script

--- Customer/Gemini-Chef.sh
+++ Customer/Gemini-Chef.sh
@@ -5,59 +5,16 @@ read -p "Do you want to continue Deploy Chef Container ? y or n " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-#  echo "Login to the Gemini Repo"
-#  if !(docker login https://registry.gemini-systems.net/)
-#  then
-#	echo "Try Running Again with Corrrect Login Credentials. ERROR: Login Failed...Exiting..."
-#	exit
-#  fi         
   echo "Pull Chef Server from Registry..."
   docker pull registry.gemini-systems.net/gemini/gemini-chef
   echo "Continue to run chef ..."
   if docker ps -a |grep -aq gemini-chef; then
         docker rm -f gemini-chef
-<<<<<<< HEAD
   fi 
   ip=`curl -s http://whatismyip.akamai.com; echo`
   printf "Enter the Host IP :"
   read -p "Default($ip):" hostip
   hostip=${hostip:-$ip}
   docker run -it -p 443:443 --privileged -v /etc/chef-server/ --name gemini-chef -h $hostip -d registry.gemini-systems.net/gemini/gemini-chef
-=======
-  fi
-<<<<<<< HEAD
-  echo "Listing the ips used in the setup..."
-#  ifconfig |grep -B1 "inet" |awk '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:" ,$1 } }' |awk -F: '{ print $1 "  " $3}'  
-  ifconfig |grep -B1 "inet"|awk '{ if ( $1 == "inet" ) { print $2 } else if ( $3 == "mtu" ) { printf "%s " ,$1 }}'; echo
-  echo "Choose one of the above accessible ips for Chef Deployment"
-  read -p  "Enter the ip:" ip
-  echo $ip
-  if [ -z $ip ]
-  then
-        printf "HostIp is Mandatory .. exiting....\n"
-        exit
-  fi
-  echo "Listing the ips used in the setup..."
-#  ifconfig |grep -B1 "inet" |awk '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:" ,$1 } }' |awk -F: '{ print $1 "  " $3}'  
-  ifconfig |grep -B1 "inet"|awk '{ if ( $1 == "inet" ) { print $2 } else if ( $3 == "mtu" ) { printf "%s " ,$1 }}'; echo
-  echo "Choose one of the above accessible ips for Chef Deployment"
-  read -p  "Enter the ip:" ip
-  echo $ip
-  if [ -z $ip ]
-  then
-        printf "HostIp is Mandatory .. exiting....\n"
-        exit
-  fi
-
-  docker run -it -p 443:443 --privileged -v /etc/chef-server/ --name gemini-chef -h $ip -d registry.gemini-systems.net/gemini/gemini-chef
->>>>>>> Updated Customer Script
-=======
- 
-  ip=`curl -s http://whatismyip.akamai.com; echo`
-  printf "Enter the Host IP :"
-  read -p "Default($ip):" hostip
-  hostip=${hostip:-$ip}
-  docker run -it -p 443:443 --privileged -v /etc/chef-server/ --name gemini-chef -h $hostip -d registry.gemini-systems.net/gemini/gemini-chef
->>>>>>> updated the way ip is got in chef deploy in customer script
 fi
 
