3fcc664077b266973662bab30b1092b25e735396
sajithgem <sajith@gemini-systems.net>
2015-10-27 08:28:21 -0400

    Added nginx

--- SPHINX/Build-Docs.sh
+++ SPHINX/Build-Docs.sh
@@ -16,4 +16,8 @@ cd /opt/SPHINX/Source
 #TODO
 #git clone 
 
-docker run -it -v /opt/SPHINX/Source:/home/SpinxDir/source -v /opt/SPHINX/Dest:/home/SpinxDir/source/_build  gemini/gemini-docs
+docker run -rm -it  -v /opt/SPHINX/Source:/home/SpinxDir/source -v /opt/SPHINX/Dest:/home/SpinxDir/source/_build  gemini/gemini-docs
+
+docker rm -f gemini-nginx
+
+docker run --name gemini-nginx -p 3000:80 -p 8443:443  -v  /opt/SPHINX/Dest/html:/usr/share/nginx/html -d nginx
