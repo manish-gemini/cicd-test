54c1f962e08302f37a793ad33406bf9e8b576ea8
Jignesh Shah <jignesh@gemini-systems.net>
2015-10-28 12:38:03 -0400

    Adding curl script to speed up download and deploy Gemini Modifying predeploy script to make it more robust

--- Customer/Gemini-predeploy.sh
+++ Customer/Gemini-predeploy.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+
 echo "Check for PreRequisite...."
 #CHECK FOR PREREQUISTE and LETS USER KNOW 
 
-#Print the Number of VCPUs
-
-echo "Number of CPU : "
-nproc
-
-echo "Total RAM Size in Bytes :"
-vmstat -s | grep 'total memory'
-
-echo "Check for Docker Version "
-if docker -v | grep 1.5 > /dev/null
-then
-	echo "Docker Version 1.5 exists"
-elif docker -v | grep 1.6 > /dev/null
-then
-	echo "Docker Version 1.6 exists"
-else 
-	yum -y update
-	yum -y install docker
-	systemctl enable docker.service
-	systemctl start docker.service
-fi
-
-echo "Setting sestatus to permissive"
+echo "Installing Gemini containers requires selinux to be turned off."
 response="y"
-read -p "Do you want to continue ? [y]/n : " -r
+read -p "Do you want to continue ? [y]/n : " -r 
 echo
 REPLY=${REPLY:-$response}
 if [[ $REPLY =~ ^[Yy] ]]
@@ -35,25 +19,45 @@ then
     setenforce 0
     echo "successfully set sestatus to permissive";
 else
-    echo "sestatus must be set to permissive for deployment."
+    echo "SELinux is currently on.1 sestatus must be set to permissive for deployment. Exiting"
     exit;
 fi 
+
 
 rpm -q ntp
 if [ $? -ne 0 ]
 then
-yum install -y ntp
+   yum install -y ntp
 fi
-echo "Time sync processing..."
+echo "Time will be synchronized with time.nist.gov for this host"
 ntpdate -b -u time.nist.gov
 echo "...."
 
-echo "Flush Iptables"
+#Print the Number of VCPUs
+echo "Number of CPU : " `nproc`
+echo "Total RAM Size in Bytes :" `vmstat -s | grep 'total memory'`
 
+echo "Check for Docker Version "
+if command_exists docker  
+then
+	echo "Docker exists:" `docker -v` "from" `rpm -qa docker`
+else 
+        echo "Docker is not installed. Installing docker..."
+	yum -y update
+	yum -y install docker-1.7
+	systemctl enable docker.service
+	systemctl start docker.service
+        if ! command_exists docker  
+        then
+           echo "Docker installation failed. Exiting."
+           exit 
+        fi
+fi
+
+echo "Flush Iptables"
 iptables -F
 
-echo "Login to Gemini Docker Registry:"
-
+echo "Login to Gemini Docker Registry using crendentials obtained from your Gemini Systems contact:"
 docker login https://registry.gemini-systems.net/
 
 
