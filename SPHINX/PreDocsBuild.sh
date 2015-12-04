db95f9d5b4e5f2537ac77f9278802a8de94810ee
sajithgem <sajith@gemini-systems.net>
2015-12-04 17:42:43 +0000

    PreDocs Build Added

--- SPHINX/PreDocsBuild.sh
+++ SPHINX/PreDocsBuild.sh
@@ -3,7 +3,8 @@
 rm -rf docs
 
 git clone https://github.com/Gemini-sys/docs.git
+cd docs
+git checkout webdocs
+cd ../docs/webdocs/
 
-cd docs/webdocs/
-
-cp -rf * IMAGE\source\*
+cp -rf * ../../IMAGE/source/
