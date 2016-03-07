70dfb8ea8379d2757b7922088e6ec08acc5ec0cc
sajithgem <sajith@apporbit.com>
2016-03-07 11:33:16 +0000

    Chef Image Update for Upgrade

--- ChefContainer/entrypoint.sh
+++ ChefContainer/entrypoint.sh
@@ -7,7 +7,7 @@ ln -sf /bin/true /sbin/initctl
 # sysctl -w kernel.shmmax=17179869184
 # Make file changes to run postgresql in less memory
 cp postgresql.rb /opt/chef-server/embedded/cookbooks/chef-server/recipes/postgresql.rb
-if [ -z $UPGRADE]
+if [ -z "$UPGRADE" ]
 then
 	export UPGRADE=0
 else
@@ -16,14 +16,16 @@ fi
  
 # Now start Chef Server
 /opt/chef-server/embedded/bin/runsvdir-start &
-chef-server-ctl reconfigure
 # UPGRADE is 2 for Upgrade to happen.
-if [ $UPGRADE == 2]
+if [ "$UPGRADE" == 2 ]
 then
-	cp -pa /var/opt/chef-server/*.json /etc/chef-server/
+    cp -pa /var/opt/chef-server/*.json /etc/chef-server/
     cp -pa /var/opt/chef-server/*.pem  /etc/chef-server/
 fi
+chef-server-ctl reconfigure
+
 cp -pa /etc/chef-server/*.json /var/opt/chef-server/
 cp -pa /etc/chef-server/*.pem /var/opt/chef-server/ 
+
 tail -F /var/log/*.log &
 chef-server-ctl tail
