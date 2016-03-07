d5101b9bd62e61f0b9d4aed7dc933454ffcc18f7
sajithgem <sajith@gemini-systems.net>
2016-03-07 12:55:55 +0530

    Image Changes

--- ChefContainer/entrypoint.sh
+++ ChefContainer/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 #
 # Entrypoint script for docker image of GeminiStack
 #
@@ -7,8 +7,23 @@ ln -sf /bin/true /sbin/initctl
 # sysctl -w kernel.shmmax=17179869184
 # Make file changes to run postgresql in less memory
 cp postgresql.rb /opt/chef-server/embedded/cookbooks/chef-server/recipes/postgresql.rb
+if [ -z $UPGRADE]
+then
+	export UPGRADE=0
+else
+	export UPGRADE=$UPGRADE
+fi
+ 
 # Now start Chef Server
 /opt/chef-server/embedded/bin/runsvdir-start &
 chef-server-ctl reconfigure
+# UPGRADE is 2 for Upgrade to happen.
+if [ $UPGRADE == 2]
+then
+	cp -pa /var/opt/chef-server/*.json /etc/chef-server/
+    cp -pa /var/opt/chef-server/*.pem  /etc/chef-server/
+fi
+cp -pa /etc/chef-server/*.json /var/opt/chef-server/
+cp -pa /etc/chef-server/*.pem /var/opt/chef-server/ 
 tail -F /var/log/*.log &
 chef-server-ctl tail
