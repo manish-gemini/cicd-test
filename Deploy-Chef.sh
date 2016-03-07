bd272b4b41d2493672e375bd817ce43908729216
sajithgem <sajith@gemini-systems.net>
2016-03-07 12:56:06 +0530

    Local deploy Changes

--- Deploy-Chef.sh
+++ Deploy-Chef.sh
@@ -9,26 +9,21 @@
 
 ##Docker Run command for Chef
 if docker ps -a |grep -aq apporbit-chef; then
-  echo "apporbit Chef Container is already runnning. The container will first be removed."
-  read -r -p "Do you want to continue? y or n " -n 1 -r installChef
-  echo    # (optional) move to a new line
-  if [[ $installChef =~ ^[Yy]$ ]]
+  echo "Install Chef or Upgrade Chef"
+  echo "1) Install Chef"
+  echo "2) Upgrade Chef"
+  read -p "Choose Chef Deployment Type from above [2]:" deployType
+  deployType=${deployType:-2}
+  if [ $deployType -eq 2 ]
   then
-   echo "Removing existing Chef Server container"
-   docker rm -f apporbit-chef 
-  else
-   echo "Exiting without installing Chef Server"
-   exit 1
+    docker rm -f apporbit-chef
   fi
 fi
-
 chef_port=9443
-
 ip=`curl -s http://whatismyip.akamai.com; echo`
 printf "Enter the Host IP or Host Name (public resolvable) :"
 read -p "Default($ip):" hostip
 hostip=${hostip:-$ip}
-
 
 # Enable the port used by Chef (permanent makes it persist after reboot)
 if [ -f /usr/bin/firewall-cmd ]
@@ -84,7 +79,7 @@ echo "Continue to run chef ..."
 
 if [ $deployType -eq 1 ]
 then
-  docker run -m 2g -it --restart=always -p $chef_port:$chef_port -v /opt/apporbit/chef-serverkey/:/var/opt/chef-server/nginx/ca/  -v /etc/chef-server/ --name apporbit-chef -h ${hostip} -d apporbit/apporbit-chef
+  docker run -m 2g -it --restart=always -e UPGRADE=$deployType -p $chef_port:$chef_port -v /opt/apporbit/chef-server:/var/opt/chef-server -v /opt/apporbit/chef-serverkey/:/var/opt/chef-server/nginx/ca/ -v /etc/chef-server/ --name apporbit-chef -h ${hostip} -d apporbit/apporbit-chef
 else
   echo "Login to the Internal Registry"
   docker login https://secure-registry.gsintlab.com
@@ -92,6 +87,6 @@ else
   docker pull secure-registry.gsintlab.com/apporbit/apporbit-chef:1.0
   echo "Please change your chef password by logging into the UI."
   echo "Using hostname: ${hostip}"
-  docker run -m 2g -it --restart=always -p $chef_port:$chef_port -v /opt/apporbit/chef-serverkey/:/var/opt/chef-server/nginx/ca/ -v /etc/chef-server/ --name apporbit-chef -h ${hostip} -d secure-registry.gsintlab.com/apporbit/apporbit-chef:1.0
+  docker run -m 2g -it --restart=always -e UPGRADE=$deployType -p $chef_port:$chef_port -v /opt/apporbit/chef-server:/var/opt/chef-server -v /opt/apporbit/chef-serverkey/:/var/opt/chef-server/nginx/ca/ -v /etc/chef-server/ --name apporbit-chef -h ${hostip} -d secure-registry.gsintlab.com/apporbit/apporbit-chef:1.0
   echo "Please change your chef password by logging into the UI."
 fi
