45c513b40dbab68025e0d5d2c46cc0d801f2839e
vikas8 <vikas@apporbit.com>
2016-10-20 09:44:34 +0000

    installer support for prometheus

--- Customer/KIT/appOrbitDeploy/src/install.sh
+++ Customer/KIT/appOrbitDeploy/src/install.sh
@@ -28,7 +28,7 @@ else
   cproxy=""
 fi
 
-FILES="action.pyc  config.pyc  README.md  userinteract.pyc  utility.pyc  apporbitlauncher.pyc  apporbit.repo apporbit-supportbundle.sh"
+FILES="action.pyc  config.pyc  README.md  userinteract.pyc  utility.pyc  apporbitlauncher.pyc  apporbit.repo apporbit-supportbundle.sh  alert.rules  alertmanager.yml  prometheus.yml"
 
 echo "Downloading apporbit installer"
 for i in $FILES
